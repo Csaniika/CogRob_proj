cca1a66a23c8171b73888ca8a5e23dc1d0dcc33d
Csaniika <harkocsanad@gmail.com>
2023-05-30 18:24:48 +0200

    added connection matrixes, configure parameter, few comment

--- scripts/set_angles.py
+++ scripts/set_angles.py
@@ -2,34 +2,51 @@
 import rospy
 import numpy as np
 from std_msgs.msg import Float64MultiArray
+import time
 
 rospy.init_node('send_joint_angles')
 
 pub = rospy.Publisher('/legs_controller/command', Float64MultiArray, queue_size=1)
-NR = 6
-w = -0.3
-tau = 0.9
-a = 0.8
-dt = 0.5
 
+NR = 6 #nr of rotators (legs)
+w = -0.5 #weight
+tau = 0.9 #time constant
+a = 0.3
+dt = 0.1
+
+#tripod
 K = [[0, 1, 1, 0, 0, 0],
      [1, 0, 0, 1, 0, 0],
      [1, 0, 0, 1, 1, 0],
      [0, 1, 1, 0, 0, 1],
      [0, 0, 1, 0, 0, 1],
      [0, 0, 0, 1, 1, 0]]
+'''
+#first two block
+K = [[0, 1, 0, 1, 1, 0],
+     [1, 0, 1, 0, 0, 1],
+     [0, 1, 0, 1, 1, 0],
+     [1, 0, 1, 0, 0, 1],
+     [1, 0, 1, 0, 0, 1],
+     [0, 1, 0, 1, 1, 0]]
 
+K = [[0, 1, 1, 0, 1, 1],
+     [1, 0, 1, 1, 0, 1],
+     [1, 1, 0, 1, 1, 0],
+     [0, 1, 1, 0, 1, 1],
+     [1, 0, 1, 1, 0, 1],
+     [1, 1, 0, 1, 1, 0]]
+'''
 
 controller_name = "legs_controller"
 joint_names = rospy.get_param("/%s/joints" % controller_name)
 rospy.loginfo("Joint names: %s" % joint_names)
 
-rate = rospy.Rate(10)
+rate = rospy.Rate(40)
 #j_c1_lf,  j_c1_rf, j_c1_lm, j_c1_rm, j_c1_lr, j_c1_rr, 
 #j_thigh_lf, j_thigh_rf, j_thigh_lm, j_thigh_rm, j_thigh_lr, j_thigh_rr, j_tibia_lf, 
 #j_tibia_rf, j_tibia_lm, j_tibia_rm, j_tibia_lr, j_tibia_rr,
  
-
 joint_positions_0 = [   np.pi/6, -np.pi/6, 0.0, 0.0, -np.pi/6, np.pi/6,
                         -np.pi/4, -np.pi/4, -np.pi/4, -np.pi/4, -np.pi/4, -np.pi/4,
                         -np.pi/4, -np.pi/4, -np.pi/4, -np.pi/4, -np.pi/4, -np.pi/4]
@@ -37,21 +54,17 @@ joint_positions_0 = [   np.pi/6, -np.pi/6, 0.0, 0.0, -np.pi/6, np.pi/6,
 joint_positions_max = joint_positions_0 + np.ones(18) * np.pi/10
 joint_positions_min = joint_positions_0 - np.ones(18) * np.pi/10
 
-#two side mirorring coxas
+#one side mirorring coxas, 
 joint_positions_max[:6:2] = joint_positions_max[:6:2] - 2 *  np.ones(3) * np.pi/10
 joint_positions_min[:6:2] = joint_positions_min[:6:2] + 2 *  np.ones(3) * np.pi/10
 
-def Phi_0values():
+def Phi_0values(): #rotator's phase
     Phi = np.zeros(6)
     for i in range(NR):
-        #if joint_positions_max[i] < joint_positions_min[i]:
-        #    joint_positions_max[i], joint_positions_min[i] = joint_positions_min[i], joint_positions_max[i]
-        #if joint_positions_max[i+NR] < joint_positions_min[i+NR]:
-        #    joint_positions_max[i+NR], joint_positions_min[i+NR] = joint_positions_min[i+NR], joint_positions_max[i+NR]
         Phi[i] = np.arccos(2 * (joint_positions_0[i]-joint_positions_min[i]) / np.abs(joint_positions_max[i] - joint_positions_min[i])) 
     return Phi
 
-def equation(i):
+def equation(i): #see readme
     global K,Phi
     part1 = 1 + a * np.cos(Phi[i])
     part2 = 0
@@ -61,11 +74,11 @@ def equation(i):
 
     return (part1 + part2) / tau
 
-def euler(dt, i):
+def euler(dt, i): #simple euler method
     dPhi = dt * equation(i)
     return dPhi
 
-def phase2rad(i, shift):
+def phase2rad(i, shift): #convert phases to min max interval
     if i < 6:
         if i%2 == 0: #coxa where min>max
             return joint_positions_min[i] - (1 + np.cos(Phi[i] + shift)) * np.abs(joint_positions_min[i]-joint_positions_max[i]) / 2    
@@ -76,19 +89,14 @@ def phase2rad(i, shift):
 
 Phi = Phi_0values()
 
-print(joint_positions_min)
-print(joint_positions_max)
-
-
-import time
-
-
+'''
 t_end = time.time() + 1
 while time.time() < t_end:
     pub.publish(Float64MultiArray(data=joint_positions_min))
 t_end = time.time() + 1
 while time.time() < t_end:
     pub.publish(Float64MultiArray(data=joint_positions_max))
+'''
 t_end = time.time() + 5
 while time.time() < t_end:
     pub.publish(Float64MultiArray(data=joint_positions_0))
@@ -101,9 +109,7 @@ while not rospy.is_shutdown():
         #print(Phi)
     for i in range(NR):
         joint_positions_next[i] = phase2rad(i, -3*np.pi/2)
-        joint_positions_next[i+NR] = phase2rad(i+NR, 0)#3*np.pi/2)
-        #print(joint_positions_next)
+        joint_positions_next[i+NR] = phase2rad(i+NR, 0)
         #joint_positions_next[i+2*NR] = phase2rad(i, 6*np.pi/4)
-    #print(joint_positions_next[6:12])
     pub.publish(Float64MultiArray(data=joint_positions_next))
     rate.sleep()
